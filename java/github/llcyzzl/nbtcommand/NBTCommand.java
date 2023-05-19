2ac79e4211c36d08f0851cf8df57c0d154391ba8
BuildTools <unconfigured@null.spigotmc.org>
2023-05-20 00:18:12 +0800

    Initial commit

---
+++ java/github/llcyzzl/nbtcommand/NBTCommand.java
@@ -0,0 +1,10 @@
+package github.llcyzzl.nbtcommand;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NBTCommand implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
